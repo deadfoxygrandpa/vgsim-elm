9650d6855a4960466e45f22898de4aabf423e0ab
stepvhen <stepvhen.web@gmail.com>
2014-04-02 03:19:08 -0400

    Made TODO file, cleaned up elm, made a compiled version.

--- Main.elm
+++ Main.elm
@@ -1,10 +1,12 @@
 import Mouse
+import Keyboard
 import Random
 import Window
 
 {--------------- INPUT -------------}
 type Input = { flips:Int
              , mpos:(Int, Int)
+             , space:Bool
              , rando:Time
              , delta:Time }
 
@@ -19,12 +21,16 @@ randogen : Signal Time
 randogen = Random.float randSig |> lift (\x -> (x * 4) + 0.51 )
 
 randSig : Signal Time
-randSig = merge (every second) (toFloat <~ count Mouse.clicks)
+randSig = merge (every second) <| toFloat <~ count Mouse.clicks
+
+spacebar : Bool -> Bool -> Bool
+spacebar hit after = hit && after
 
 input : Signal Input
 input = sampleOn tdelta <| 
   Input <~ count Mouse.clicks
          ~ Mouse.position
+         ~ (spacebar <~ Keyboard.space ~ ((second *0.03) `since` Keyboard.space))
          ~ randogen
          ~ tdelta
    
@@ -53,6 +59,7 @@ defaultPlayer = { timeOn = 0.0
 
 type GameState = { lstate:Lstate
                  , gstate:Gstate
+                 , paused:Bool
                  , gflips:Int
                  , timer:Time
                  , grando:Time
@@ -61,6 +68,7 @@ type GameState = { lstate:Lstate
 defaultGame : GameState
 defaultGame = { lstate = On
               , gstate = Start
+              , paused = False
               , gflips = 0
               , timer = 0.0
               , grando = 5.0 
@@ -74,7 +82,6 @@ toggle : Lstate -> Lstate
 toggle l = if | l == On -> Off
               | l == Off -> On
               | otherwise -> l
-
 {--
   Light state changing logic 
   The bulk of the game. Manages light state based on
@@ -87,36 +94,42 @@ lightSwitch {lstate, gstate} flipped timed =
     case gstate of
       Start -> if timed then toggle lstate else lstate
       Skill -> if | lstate == Off && timed  -> toggle lstate
-                | lstate == On && flipped -> toggle lstate
-                | otherwise -> lstate
+                  | lstate == On && flipped -> toggle lstate
+                  | otherwise -> lstate
       _     -> toggle lstate
 
 -- Method for recording game progress. 
 stepPlayer : Player -> Lstate -> Time -> Bool -> Player
 stepPlayer ({ timeOn, timeOff, pflips } as player)
            lstate delta flipped =
-    let timeOn' = if | lstate == On   -> timeOn + delta
-                     | otherwise      -> timeOn
-        timeOff'= if | lstate == Off  -> timeOff + delta
-                     | otherwise      -> timeOff
+    let timeOn' = if lstate == On  then timeOn + delta else timeOn
+        timeOff'= if lstate == Off then timeOff + delta else timeOff
         pflips' = if flipped then pflips + 1 else pflips
      in { player | timeOn  <- timeOn'
                  , timeOff <- timeOff'
                  , pflips  <- pflips' }
 
+-- Primary update layer, allows for pausing of game. 
+pauseGame : Input -> GameState -> GameState
+pauseGame ({ space } as input) ({paused} as game) =
+  let paused' = if space then (not paused) else paused
+  in if (not paused') 
+     then stepGame input {game| paused <- paused'} 
+     else {game| paused <- paused'}
+     
 -- Game state logic
 stepGame : Input -> GameState -> GameState
-stepGame { flips, mpos, rando, delta }
-         ({ lstate, gstate, gflips, timer, grando, player } as game) =
+stepGame { flips, mpos, space, rando, delta }
+         ({ lstate, gstate, paused, gflips, timer, grando, player } as game) =
     let state' = lightSwitch game (flips > gflips) (timer > grando)
-        gstate' = Start
+        gstate' = gstate
         gflips' = flips      
         timer' = if state' /= lstate then 0     else timer + delta
         grando'= if state' /= lstate then rando else grando
         player'= if gstate' /= gstate 
                  then defaultPlayer
-                 else stepPlayer player lstate delta (state' /= lstate)       
-    in {game | lstate <- state'
+                 else stepPlayer player lstate delta (state' /= lstate) 
+     in {game| lstate <- state'
              , gstate <- gstate'
              , gflips <- gflips'
              , timer  <- timer'
@@ -130,12 +143,14 @@ display : (Int, Int) -> Input -> GameState -> Element
 display (w,h) 
         (inout)
         ({lstate} as game) = 
-  let light = if | lstate == Off -> square 300 |> filled black
-                 | otherwise     -> square 300 |> filled white
+  let w' = toFloat w
+      h' = toFloat h
+      light = if | lstate == Off -> rect w' h' |> filled black
+                 | otherwise     -> rect w' h' |> filled white
    in flow down [
-                 asText inout, asText game, 
-                 collage 300 300 [light]
+                 --asText inout, asText game, 
+                 collage w h [light, (toForm <| plainText "video game simulator\nby stepvhen")]
                  ]
 
-gameState = foldp stepGame defaultGame input
+gameState = foldp pauseGame defaultGame input
 main = lift3 display Window.dimensions input gameState
